0ea19e1569c5e5d8fce93a1668d9b6a6e1a8f240
Muhammad Usman <contact@mhmd.us>
2022-12-27 13:47:28 -0800

    two real playlists

--- src/lib/Playlists.svelte
+++ src/lib/Playlists.svelte
@@ -2,18 +2,32 @@
   import playlists from "../data/playlists.json";
   // imports playlist.css
   import "../styles/playlist.css";
+
+  const handleClick = (e, playlist) => {
+    location = playlist.link;
+  };
+
+  const handleNothing = (e) => {
+    console.log(e);
+  };
 </script>
 
 <grid>
   {#each playlists as playlist}
-    <card class="playlist-card">
+    <card
+      class="playlist-card"
+      on:click={(e) => {
+        handleClick(e, playlist);
+      }}
+      on:keypress={handleNothing}
+    >
       <flex>
         <left>
           <img src={playlist.image} alt={playlist.name} />
         </left>
         <right
           ><h2>{playlist.name}</h2>
-          <p>{playlist.description}</p></right
+          <p class="description">{playlist.description}</p></right
         >
       </flex>
     </card>
